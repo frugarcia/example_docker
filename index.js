0a7e6e3dc402b9fe10bd8e56c48e2ee57e1973b6
frugarcia <frugarcia@gmail.com>
2021-06-27 20:20:07 +0200

    Example git

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.HTTP_SERVER_PORT || 3000;
 
 console.log("MENSAJE DE PRUEBA DE INICIO DE SCRIPT");
+console.log("MENSAJE DE PRUEBA DE INICIO DE SCRIPT");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
